b0c1419bad7b074163b3203caeb80d8af10a0be7
ana <111012665+aanamartinss@users.noreply.github.com>
2022-09-19 14:47:44 -0300

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,9 @@
+
+N1 = int(input ('Digite o primeiro número:'))
+N2 = int(input ('Digite o segundo número:'))
+prod = N1 + N2
+total = N1 + N2
+if total > 10:
+    print("total :", total)
+else:
+  print("a soma da resultado inferior a 10")
